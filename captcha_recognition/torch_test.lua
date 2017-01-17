a53fc860182b76feafc4d86b2e8513de0bf2247f
hemaoliang <h02095120@163.com>
2017-01-17 00:12:43 -0500

    add torch test

---
+++ captcha_recognition/torch_test.lua
@@ -0,0 +1,28 @@
+require 'nn'
+require 'paths'
+
+if (not paths.filep("/home/tracyhe/data_download/cifar10torchsmall.zip")) then
+    os.execute('wget -c https://s3.amazonaws.com/torch7/data/cifar10torchsmall.zip')
+    os.execute('unzip cifar10torchsmall.zip')
+end
+trainset = torch.load('/home/tracyhe/data_download/cifar10-train.t7')
+testset = torch.load('/home/tracyhe/data_download/cifar10-test.t7')
+classes = {'airplane', 'automobile', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
+
+
+-- ignore setmetatable for now, it is a feature beyond the scope of this tutorial. It sets the index operator.
+setmetatable(trainset, 
+    {__index = function(t, i) 
+                    return {t.data[i], t.label[i]} 
+                end}
+);
+trainset.data = trainset.data:double() -- convert the data from a ByteTensor to a DoubleTensor.
+
+function trainset:size() 
+    return self.data:size(1) 
+end
+
+print(trainset:size()) -- just to test
+
+
