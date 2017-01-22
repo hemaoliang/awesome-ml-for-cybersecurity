e0e8862cfae0c3408768ae0dec9fb3da3c2d9899
hemaoliang <h02095120@163.com>
2017-01-21 22:11:10 -0500

    add dropout layer

--- captcha_recognition/captcha.lua
+++ captcha_recognition/captcha.lua
@@ -2,16 +2,27 @@ local dir = '/home/tracyhe/data_download/simplecaptcha_generate/'
 local validationSize = 1000
 local iterations = 30
 local batchSize = 32
+--local sgd_config = {
+--      learningRate = 0.1,
+--      learningRateDecay = 5.0e-6,
+--      momentum = 0.9
+--   }
+
 local sgd_config = {
-      learningRate = 0.1,
-      learningRateDecay = 5.0e-6,
+      learningRate = 0.01,
+      learningRateDecay = 1.0e-4,
       momentum = 0.9
    }
 
 local data = require 'data';
 --第一次运行需要执行storeXY(dir)
-data.storeXY(dir)
-local X,Y = data.loadXY(dir)
+--data.storeXY(dir)
+--local X,Y = data.loadXY(dir)
+
+print('loading data begin..')
+
+local Y = data.loadY(dir)
+local X = data.loadX(dir,Y:size(1),50,200)
 local Xt,Yt,Xv,Yv = data.split(X,Y,validationSize)
 
 print('loading data end..')
@@ -35,7 +46,7 @@ local train = require 'train';
 
 train.sgd(net,ct,Xt,Yt,Xv,Yv,iterations,sgd_config,batchSize)
 
-torch.save(dir .. 'net.t7',net)
+--torch.save(dir .. 'net.t7',net)
 
 --print('valid .. '.. train.accuracy(Xv,Yv,net,batchSize))
 --print('valid .. '.. train.accuracyK(Xv,Yv,net,batchSize))
